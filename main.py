59176eb846036772d6c2d7f06a0fa4ddee5c195c
F1cky <97504239+F1cky@users.noreply.github.com>
2022-02-08 14:40:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# - *- coding: utf- 8 - *-
+async def on_startup(dp):
+    import asyncio
+    from utils.other_func import on_startup_notify, update_last_profit, check_update_bot, update_profit
+    from utils.set_bot_commands import set_default_commands
+    from utils.db_api.sqlite import create_bdx
+
+    await set_default_commands(dp)
+    await on_startup_notify(dp)
+    create_bdx()
+    update_profit()
+    print("~~~~~ Bot was started ~~~~~")
+    asyncio.create_task(update_last_profit())
+    asyncio.create_task(check_update_bot())
+
+
+if __name__ == "__main__":
+    from aiogram import executor
+    from handlers import dp
+    import filters
+    import middlewares
+    filters.setup(dp)
+    middlewares.setup(dp)
+
+    executor.start_polling(dp, on_startup=on_startup)
